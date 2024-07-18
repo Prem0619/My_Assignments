2421901fe6096a9692889967d1f31834eb1ca567
Prem0619 <premsuresh19@gmail.com>
2024-07-18 21:24:25 +0530

    Add files via upload
    day2

---
+++ day2/Chrome.java
@@ -0,0 +1,26 @@
+package week1.day2;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float version = 91.0f;
+		String developer = "Google";
+		boolean beta = false;
+		int releaseYearOfType = 2008;
+		char shortCutKey = 'c';
+		
+		System.out.println(version);
+		System.out.println(developer);
+		System.out.println(beta);
+		System.out.println(releaseYearOfType);
+		System.out.println(shortCutKey);
+	}	
+	
+}
+
+
+
+		
+		
